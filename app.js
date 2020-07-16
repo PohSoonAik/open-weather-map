c2b6d4cbe8e0805b5607ce16d9cec36ac12ae1d9
unknown <psa_1105@hotmail.com>
2020-07-16 18:45:13 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+var input = document.querySelector('.input_text');
+var main = document.querySelector('#name');
+var temp = document.querySelector('.temp');
+var desc = document.querySelector('.desc');
+var clouds = document.querySelector('.clouds');
+var button= document.querySelector('.submit');
+
+
+button.addEventListener('click', function(name){
+fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=8f5d916cc4ea5932c371a57de5dc6c81')
+.then(response => response.json())
+.then(data => {
+  var tempValue = data['main']['temp'];
+  var nameValue = data['name'];
+  var descValue = data['weather'][0]['description'];
+
+  main.innerHTML = nameValue;
+  desc.innerHTML = "Cloud situation - "+descValue;
+  temp.innerHTML = "Temperature - "+(tempValue/10);
+  input.value ="";
+
+})
+
+.catch(err => alert("Wrong city name!"));
+})
